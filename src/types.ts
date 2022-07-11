6c6f357ec8c0806f8b9f5be28394f77e79e9b3e2
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-07-11 10:51:15 -0500

    overhauled type system

--- src/types.ts
+++ src/types.ts
@@ -1,27 +1,22 @@
-import { Board } from "./board";
-
 // arbitrarily chose max 20 columns
 // prettier-ignore
 export type Column = 'a'|'b'|'c'|'d'|'e'|'f'|'g'|'h'|'i'|'j'|'k'|'l'|'m'|'n'|'o'|'p'|'q'|'r'|'s'|'t'
 // prettier-ignore
 export type Row = '1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'|'10'|'11'|'12'|'13'|'14'|'15'|'16'|'17'|'18'|'19'|'20';
-export type Position = `${Column}${Row}`;
-
-export type Comment = string;
+export type Square = `${Column}${Row}`;
 export type Player = "w" | "b";
-export type FEN = `${string} ${Player} ${Position | "-"} ${number}`;
-export type Header = { [header: string]: string };
-export type Move = string;
-export type PGN = string[];
+export type FEN = `${string} ${Player} ${Square | "-"} ${number}`;
+export type Move = string; //`${Square}-${Square}-${Square}`;
+export type HalfMove = string;
 
-export const enum PosColor {
+export const enum SqColor {
   /** h1 and a8 are light squares */
   LIGHT,
   /** a1 and h8 are dark squares */
   DARK,
 }
 
-export const enum Square {
+export const enum Piece {
   /** Spot is valid for moving to. */
   EMPTY,
   /** A white piece occupys this spot. */
@@ -32,27 +27,14 @@ export const enum Square {
   ARROW,
 }
 
-/* export interface BoardType {
-  rows: number;
-  cols: number;
-  board: Square[][];
-  get: (pos: Position) => Square;
-  get_index: (pos: Position) => [number, number];
-  move: (move: Move) => void;
-  undo: (move: Move) => void;
-} */
+export type Board = Piece[][];
 
-export type ErrorType = {
-  error: string;
-};
-
-export interface Game {
-  layout: string;
-  moving: Player;
-  shooting: Position;
-  turn: number;
+export type State = {
   board: Board;
-}
+  moving: Player;
+  shooting: Square | null;
+  turn: number;
+};
 
 /* export interface Amazons {
   ascii: () => string;
