63c00b1bc8e825302dbb2b8fc3b485a5f1d2104e
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-07-11 19:59:00 -0500

    feat: added some methods to engine.ts

--- src/engine.ts
+++ src/engine.ts
@@ -1,4 +1,4 @@
-import { COLS, MAP_COLOR_SQUARE } from "./consts";
+import { BLACK, COLS, PLAYER_TO_PIECE, WHITE } from "./consts";
 import { FEN, HalfMove, Move, Piece, Player, Square, State } from "./types";
 import {
   board_to_layout,
@@ -29,15 +29,43 @@ export class Engine {
     }
   }
 
-  public move(m: HalfMove) {
-    //TODO
+  /**
+   * Update state by preforming half move m.
+   * DOES NOT validate. Blindly trusts that m is part of available moves
+   */
+  public move(m: HalfMove | Square) {
+    if (this.state.shooting) {
+      this.put(m as Square, Piece.ARROW);
+      this.state.turn++;
+      this.change_moving_player();
+      this.state.shooting = null;
+    } else {
+      let [from, to] = m.split("-");
+      this.put(from as Square, Piece.EMPTY);
+      this.put(to as Square, PLAYER_TO_PIECE[this.state.moving]);
+      this.state.shooting = to as Square;
+    }
   }
-  public full_move(m: Move) {
-    //TODO
-  }
+
+  /**
+   * Update state by preforming full move m.
+   * DOES NOT validate. Blindly trusts that m is part of available moves
+   */
+  public full_move(m: Move) {}
   public get_moves() {
-    // TODO
+    if (this.state.shooting) {
+      return this.get_vision(this.state.shooting);
+    }
+
+    let ans: HalfMove[] = [];
+    for (let queen of this.get_location_of(this.state.moving)) {
+      for (let sq of this.get_vision(queen)) {
+        ans.push(`${queen}-${sq}`);
+      }
+    }
+    return ans;
   }
+
   public undo(m: HalfMove) {
     //TODO
   }
@@ -47,6 +75,7 @@ export class Engine {
   public get_board() {
     //TODO
   }
+
   public get(sq: Square): Piece {
     let [row, col] = this.get_index(sq);
     return this.state.board[row][col];
@@ -58,6 +87,43 @@ export class Engine {
   }
 
   /**************** PRIVATE METHODS *******************/
+  private get_location_of(p: Player) {
+    let piece = PLAYER_TO_PIECE[p];
+    let squares: Square[] = [];
+    for (let r = 0; r < this.rows; r++)
+      for (let c = 0; c < this.cols; c++)
+        if (this.state.board[r][c] === piece)
+          squares.push(this.from_index(r, c));
+    return squares;
+  }
+
+  private get_vision(sq: Square) {
+    let squares: Square[] = [];
+    for (let i of [-1, 0, 1]) {
+      for (let j of [-1, 0, 1]) {
+        if (i === 0 && j === 0) continue;
+
+        let [r, c] = this.get_index(sq);
+        r += i;
+        c += j;
+        while (this.state.board[r] && this.state.board[r][c] === Piece.EMPTY) {
+          squares.push(this.from_index(r, c));
+          r += i;
+          c += j;
+        }
+      }
+    }
+    return squares;
+  }
+
+  private change_moving_player() {
+    this.state.moving = this.state.moving === BLACK ? WHITE : BLACK;
+  }
+
+  private put(sq: Square, p: Piece) {
+    let [r, c] = this.get_index(sq);
+    this.state.board[r][c] = p;
+  }
 
   private make_state_from_fen(fen: FEN) {
     let temp = is_fen(fen, true);
@@ -99,6 +165,10 @@ export class Engine {
     return indexes;
   }
 
+  private from_index(row: number, col: number): Square {
+    return `${COLS[col]}${row + 1}` as Square;
+  }
+
   private shooting_is_valid() {
     if (this.state.shooting === null) return { error: null };
 
@@ -106,7 +176,7 @@ export class Engine {
       return { error: "Shooting field is not a valid Square" };
 
     // otherwise, check if shooting is the correct color
-    if (this.get(this.state.shooting) !== MAP_COLOR_SQUARE[this.state.moving])
+    if (this.get(this.state.shooting) !== PLAYER_TO_PIECE[this.state.moving])
       return { error: "shooting position does not point to moving player" };
 
     return { error: null };
