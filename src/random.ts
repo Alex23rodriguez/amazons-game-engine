4308d09d1bda14245a8243836a5a294f2ac2f8a3
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-07-13 14:44:49 -0500

    feat: file to test out random game!

---
+++ src/random.ts
@@ -0,0 +1,16 @@
+import { DEFAULT_POSITIONS } from "./consts";
+import { Engine } from "./engine";
+
+let e = new Engine(DEFAULT_POSITIONS[6]);
+
+console.log("start!");
+console.log(e.ascii());
+let moves = e.moves();
+while (moves.length) {
+  let m = moves[Math.floor(Math.random() * moves.length)];
+  console.log("\n", e.move_num, e.turn, "shooting: ", !!e.shooting_sq);
+  e.move(m);
+  moves = e.moves();
+  console.log(e.ascii());
+}
+console.log(`${e.turn} loses!`);
