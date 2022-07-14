fb52561846623408f8473f76917868a86cb752c9
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-07-13 19:23:12 -0500

    feat: added undo, half_undo, put and get methods, as well as did some renaming

--- src/engine.ts
+++ src/engine.ts
@@ -1,5 +1,5 @@
 import { Board } from "./board";
-import { LAYOUT_MAP, P_BLACK, P_WHITE } from "./consts";
+import { LAYOUT_MAP, P_BLACK, P_WHITE, NOT_SHOOTING } from "./consts";
 import { FEN, Move, Piece, Player, Square } from "./types";
 
 export class Engine {
@@ -11,22 +11,23 @@ export class Engine {
   private hist: Move[];
 
   constructor(fen: FEN) {
-    let [layout, turn, shooting, move_num] = fen.split(/\s+/);
+    let [layout, turn, shooting_sq, move_num] = fen.split(/\s+/);
     this._board = new Board(layout);
     this._turn = turn as Player;
-    this._shooting_sq = shooting === "-" ? null : (shooting as Square);
+    this._shooting_sq =
+      shooting_sq === NOT_SHOOTING ? null : (shooting_sq as Square);
     this._move_num = Number(move_num);
 
     this.hist = [];
   }
 
-  public fen() {
+  fen() {
     return `${this._board.layout()} ${this._turn} ${this._shooting_sq} ${
       this._move_num
     }`;
   }
 
-  public move(m: Move) {
+  move(m: Move) {
     // move board
     this._board.move(...m);
 
@@ -38,7 +39,46 @@ export class Engine {
     ](...m);
   }
 
-  public moves(): Move[] {
+  undo() {
+    let m = this.hist.pop();
+    if (typeof m === "undefined") return null;
+
+    this._board.undo(...m);
+
+    if (m.length > 1) {
+      this.undo_move();
+      return m;
+    }
+
+    // m.length === 1
+    // this should only happen if starting position was shooting
+    this.undo_shooting(m[0]);
+    return m;
+  }
+
+  half_undo(): Move {
+    let m = this.hist.pop();
+    if (typeof m === "undefined") return null;
+
+    if (m.length === 3) {
+      this._board.undo(m[2]);
+      this.undo_shooting(m[2]);
+      this.hist.push([m[0], m[1]]);
+      return [m[2]];
+    }
+
+    if (m.length === 2) {
+      this.undo_move();
+      return m;
+    }
+
+    // m.length === 1
+    // this should only happen if starting position was shooting
+    this.undo_shooting(m[0]);
+    return m;
+  }
+
+  moves(): Move[] {
     if (this._shooting_sq) {
       let queen_vision = this._board.get_vision(this._shooting_sq);
       return queen_vision.map((s) => [s]);
@@ -61,6 +101,15 @@ export class Engine {
     return this._board.ascii();
   }
 
+  put(piece: Piece, sq: Square) {
+    this._board.put(piece, sq);
+    this.hist = []; // clear history, otherwise will get errors
+  }
+
+  get(sq: Square) {
+    return this._board.get(sq);
+  }
+
   // GETTERS
 
   get turn() {
@@ -76,11 +125,23 @@ export class Engine {
   }
   // PRIVATE METHODS
 
+  private switch_player() {
+    this._turn = this._turn === P_WHITE ? P_BLACK : P_WHITE;
+  }
+
   // notice that in these functions, arrow syntaxt is important for binding 'this'
+  private after_queen_move = (sq1: Square, sq2: Square) => {
+    // update state
+    this._shooting_sq = sq2;
+
+    // update history
+    this.hist.push([sq1, sq2]);
+  };
+
   private after_shooting = (sq3: Square) => {
     // update state
-    this._move_num++;
     this.switch_player();
+    if (this._turn === P_WHITE) this._move_num++;
     this._shooting_sq = null;
 
     // update history
@@ -92,25 +153,23 @@ export class Engine {
     }
   };
 
-  private after_queen_move = (sq1: Square, sq2: Square) => {
-    // update state
-    this._shooting_sq = sq2;
-
-    // update history
-    this.hist.push([sq1, sq2]);
-  };
-
   private after_full_move = (sq1: Square, sq2: Square, sq3: Square) => {
     // update state
-    this._move_num++;
     this.switch_player();
+    if (this._turn === P_WHITE) this._move_num++;
     // shooting is already null
 
     // update history
     this.hist.push([sq1, sq2, sq3]);
   };
 
-  private switch_player() {
-    this._turn = this._turn === P_WHITE ? P_BLACK : P_WHITE;
+  private undo_shooting(sq3: Square) {
+    this._shooting_sq = sq3;
+  }
+
+  private undo_move() {
+    this.switch_player();
+    if (this._turn === P_BLACK) this._move_num--;
+    this._shooting_sq = null;
   }
 }
